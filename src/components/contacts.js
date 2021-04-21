ccb1519bcedfd7b4480492fde3f9a85decd327b6
madhuri <madhuri.djs3@gmail.com>
2021-04-21 15:44:47 +0530

    get and displaydetails from  firebaseDB

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -3,7 +3,17 @@ import Contactform from './contactform';
 import firebaseDB from "../firebase";
 
 const Contacts = () => {
+    const [contactObjects, setContactObjects] = useState({});
 
+    useEffect(() => {
+        firebaseDB.child("contacts").on("value", snapshot => {
+            if(snapshot.val() !== null ){
+                setContactObjects({
+                    ...snapshot.val()
+                })
+            }
+        })
+    })
     const addOrEdit = obj => {
         firebaseDB.child("contacts").push(
             obj,
@@ -27,7 +37,37 @@ const Contacts = () => {
                     <Contactform addOrEdit= {addOrEdit}/>
                 </div>
                 <div className= "col-md-7">
-                    display contacts
+                   <table className= "table table-borderless table-stripped">
+                       <thead className= "thead-light">
+                           <tr>
+                               <th>Full Name</th>
+                               <th>Mobile</th>
+                               <th>Email</th>
+                               <th>Actions</th>
+                           </tr>
+                       </thead>
+                       <tbody>
+                           {
+                               Object.keys(contactObjects).map((id) => {
+                                   return(
+                                       <tr>
+                                           <td>{contactObjects[id].fullName}</td>
+                                           <td>{contactObjects[id].mobile}</td>
+                                           <td>{contactObjects[id].email}</td>
+                                           <td>
+                                               <a className= "btn text-primary">
+                                                    <i className= "fas fa-pencil-alt"></i>
+                                               </a>
+                                               <a className= "btn text-danger">
+                                                    <i className= "fas fa-trash-alt"></i>
+                                               </a>
+                                           </td>
+                                       </tr>
+                                   )
+                               })
+                           }
+                       </tbody>
+                   </table>
                 </div>
             </div>
         </>
